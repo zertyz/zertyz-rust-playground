f25a2de93a4b12dc948785ff16f4feaec167fdb2
Luiz Silveira <zertyz@gmail.com>
2021-11-21 01:56:28 -0300

    web-backend-rocket: added a bootstrap theme

--- web-backend-rocket/build.rs
+++ web-backend-rocket/build.rs
@@ -8,8 +8,9 @@ use std::{
     process::Command,
     collections::HashMap,
     time::{SystemTime,Duration},
+    ops::Add,
 };
-use std::ops::Add;
+use walkdir::WalkDir;
 use chrono::{DateTime, Utc};
 use flate2::{
     GzBuilder,
@@ -46,7 +47,8 @@ fn on_non_release() {
     );
 }
 
-/// builds the angular site for production, then loads (and compresses) the resulting static files, storing them in a hash map for use by the application
+/// builds the angular site for production, then loads (and compresses) the resulting static files, storing them in a hash map for use by the application.\
+///
 fn on_release() {
     let angular_relative_path = "./angular";
     let angular_dist_path = format!("{}/dist/DemoAngularAndBootstrapUI", angular_relative_path);
@@ -65,27 +67,30 @@ fn on_release() {
         .spawn().expect("Failed to start Angular UI Application!")
         .wait().unwrap();
 
+    // reads all static files, recursively
     let mut files_contents = HashMap::<String, Vec<u8>>::new();
     let mut current_dir = env::current_dir().unwrap();
     current_dir = current_dir.join(angular_dist_path);
     let root_dir = PathBuf::from(&current_dir);
     eprintln!("Incorporating all files from '{:?}' into the executable", root_dir);
+    WalkDir::new(current_dir)
+        .into_iter()
+        .filter_entry(|entry| entry
+            .file_name()
+            .to_str()
+            .map(|entry_name| entry_name != "." && entry_name != "..")
+            .unwrap_or(false))
+        .filter_map(|v| v.ok())
+        .for_each(|entry| {
+            let metadata = fs::metadata(&entry.path()).unwrap();
+            if metadata.is_file() {
+                let file_contents = fs::read(&entry.path()).expect(&format!("Cannot read file contents: '{:?}'", entry));
+                let relative_file_name = entry.path().to_string_lossy().to_string().replace(root_dir.to_str().unwrap(), "");
+                files_contents.insert(relative_file_name, file_contents);
+            }
+        });
 
-    // reads all static files
-    for entry in fs::read_dir(&current_dir).expect(&format!("Cannot traverse directory '{:?}", &current_dir)) {
-        let entry = entry.unwrap();
-        let path = entry.path();
-
-        let metadata = fs::metadata(&path).unwrap();
-
-        if metadata.is_file() {
-            let file_contents = fs::read(&path).expect(&format!("Cannot read file contents: '{:?}'", path));
-            let relative_file_name = path.to_string_lossy().to_string().replace(root_dir.to_str().unwrap(), "");
-            files_contents.insert(relative_file_name, file_contents);
-        }
-    }
-
-    // includes all angular routes as static files (downloading index.html)
+    // includes all angular routes as links to index.html
     let output = Command::new(shell)
         .args(["-c", &get_angular_routes_command])
         .output().expect("Failed to start Angular UI Application!")
@@ -100,8 +105,9 @@ fn on_release() {
     save_static_files(files_contents, file_links);
 }
 
-/// saves (possibly compressing) 'static_files' into a const hash map for use by the application.\
-/// 'file_links' := {link_name = real_file_name, ...}
+/// saves (possibly compressing) 'static_files' into a const hash map for use by the web server & application
+/// when clients request them. Additionally, defines some date constants useful for optimizing the browser's cache.\
+/// 'file_links' refers to 'static_files' in the form {link_name = real_file_name, ...}\
 fn save_static_files(static_files: HashMap<String, Vec<u8>>, file_links: HashMap<String, String>) {
     const CACHE_MAX_AGE_SECONDS: u64 = 31536000;
     let out_dir = env::var_os("OUT_DIR").unwrap();
@@ -172,11 +178,11 @@ lazy_static! {
     writer.write(hash_map_header.as_bytes() ).unwrap();
 
     // contents (hash map)
-    writer.write("        // links\n".as_bytes());
+    writer.write("        // links\n".as_bytes() ).unwrap();
     for (link_name, real_file_name) in &file_links {
         writer.write(format!("        m.insert(\"{}\", {});\n", link_name, file_name_as_token(real_file_name)).as_bytes() ).unwrap();
     }
-    writer.write("        // files\n".as_bytes());
+    writer.write("        // files\n".as_bytes() ).unwrap();
     for (file_name, file_contents) in &static_files {
         writer.write(format!("        m.insert(\"{}\", {});\n", file_name, file_name_as_token(file_name)).as_bytes() ).unwrap();
     }
