9583751afdf1ffc15fa1045fb11deffac20d67a1
Luiz Silveira <zertyz@gmail.com>
2022-05-16 22:42:44 -0300

    db spikes version updates

--- embedded-databases/src/embedded-databases.rs
+++ embedded-databases/src/embedded-databases.rs
@@ -1,13 +1,61 @@
-use std::error::Error;
 
-fn main() -> Result<(), StructsyError> {
+fn main() {
     eprintln!("##############################");
     eprintln!("## embedded DATABASE CRATES ##");
     eprintln!("##############################");
 
-    structsy_spikes();
-    sled_spikes();
-    Ok(())
+    //structsy_spikes().unwrap();
+    //sled_spikes();
+    redb_spikes();
+}
+
+use redb::{Database, Error, ReadableTable, TableDefinition};
+
+const TABLE: TableDefinition<str, str> = TableDefinition::new("my_data");
+
+fn redb_spikes() {
+    eprintln!();
+    eprintln!("## 'redb' crate:");
+    eprintln!("####################");
+    eprintln!("# Faster then sled?");
+
+    let db = unsafe { Database::create("/tmp/redb.redb", 1024 * 1024).expect("db open") };
+
+    // insert
+    let write_txn = db.begin_write().expect("write transaction start");
+    let mut table = write_txn.open_table(TABLE).expect("write handle");
+    let num_elements = 100000;
+    for i in 0..num_elements {
+        let name = i.to_string();
+        let address = format!("https://gitlab.com/tglman/structsy for {}", i);
+        table.insert(&name, &address).expect("insert");
+        if i % (num_elements/100) == 0 {
+            println!("{}%, ", (i*100)/num_elements);
+        }
+    }
+    println!("100% done. Flushing...");
+    drop(table);
+    write_txn.commit().expect("commit");
+    println!("Committed. Searching for the last element on the indexed field...");
+
+    // retrieve
+    let read_txn = db.begin_read().expect("read transaction start");
+    let table = read_txn.open_table(TABLE).expect("read handle");
+    let result_str = table.get(&(num_elements-1).to_string()).unwrap().unwrap();
+    println!("element #{}: {}", num_elements-1, result_str);
+
+    // range queries
+    println!("Traversing through all elements...");
+    let mut n = 0;
+    for kv in table.range(0.to_string()..(num_elements-1).to_string()) {
+        n = n + 1;
+    }
+    println!("Done. {} elements found!", n);
+
+    eprintln!();
+    eprintln!("# CONCLUSION: regdb... version 0.1.x, very disappointingly, only allowed 9930 records to be inserted before panicking out with 'OutOfSpace'.");
+    eprintln!("#             Not even close to be usable.");
+
 }
 
 use structsy::{Structsy, StructsyError, StructsyTx};
@@ -74,6 +122,7 @@ fn structsy_spikes() -> Result<(), StructsyError> {
     eprintln!("# CONCLUSION: structsy 0.3 is unreasonably slow to insert, even when using a single transaction, as measured");
     eprintln!("              by this 100.000 insertions test. Surprisingly, insertion is not O(1) -- looks like O(n²).");
     eprintln!("              Compiling in RELEASE mode improves times, but still don't take it to production-acceptable speeds.");
+    eprintln!("              structsy 0.4 is equally bad, regarding speed -- maybe slightly better. I just measured 1:06.682s (0.3) vs 1:0.076s (0.4)");
 
     Ok(())
 }
@@ -92,9 +141,9 @@ fn sled_spikes() {
         let name = i.to_string();
         let address = format!("https://gitlab.com/tglman/structsy for {}", i);
         tree.insert(&name, address.as_str());
-        if i % (num_elements/100) == 0 {
-            println!("{}%, ", (i*100)/num_elements);
-        }
+        // if i % (num_elements/100) == 0 {
+        //     println!("{}%, ", (i*100)/num_elements);
+        // }
     }
     println!("100% done. Flushing...");
     tree.flush();
